6926078a16c896b82169cd4551cf71ec0e981ee2
Minchan Kim <minchan@google.com>
2018-07-17 13:43:57 +0900

    mm: add ION heap-pool allocation stat into vmstat
    Android uses ION heap memory a lot. With that, we have a problem
    to estimate memory usage because VM doesn't account alloc_pages
    from kernel like ION heap.
    
    This patch adds ION heap pool alloation stat into /proc/vmstat.
    
    nr_ion_heap
    
    It's the number of pages to allocated for ION system heap
    currently. But please keep it mind. When the driver release
    ION memory, it is added into ION page pool rather than buddy
    allocator in VM. Therefore, nr_ion_heap includes both used and
    unused (nr_indirectly_reclaimable) ION pages.
    
    IOW, if you want to get exact number of ION page which is used
    by the system at this moment, you should use following expression.
    
    	nr_ion_heap * PAGE_SIZE - nr_indirectly_reclaimable
    
    Android uses ION heap memory huge so it would be really helpful
    to investigate memory loss problem when we debug memory leak or
    low memory kills of top process.
    
    Bug: 110330255
    Change-Id: I0c691ccc2315f12d25b080cd3580019c6495019d
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -976,6 +976,7 @@ const char * const vmstat_text[] = {
 	"nr_written",
 	"nr_indirectly_reclaimable",
 	"nr_gpu_heap",
+	"nr_ion_heap",
 
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
