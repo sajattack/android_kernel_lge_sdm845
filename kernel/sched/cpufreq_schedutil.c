6a25005409e2126e2573122a3754f1e29289053a
Wei Wang <wvw@google.com>
2020-06-30 13:47:39 -0700

    ANDROID: cpufreq: schedutil: maintain raw cache when next_f is not changed
    Currently, the raw cache will be reset when next_f is changed after get_next_freq for correctness. However, it may introduce more cycles in those cases. This patch changes it to maintain the cached value instead of dropping it.
    
    Bug: 159936782
    Bug: 158863204
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I519ca02dd2e6038e3966e1f68fee641628827c82

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -42,6 +42,7 @@ struct sugov_policy {
 	s64 down_rate_delay_ns;
 	unsigned int next_freq;
 	unsigned int cached_raw_freq;
+	unsigned int prev_cached_raw_freq;
 
 	/* The next fields are only needed if fast switch cannot be used. */
 	struct irq_work irq_work;
@@ -131,8 +132,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 		return;
 
 	if (sugov_up_down_rate_limit(sg_policy, time, next_freq)) {
-		/* Don't cache a raw freq that didn't become next_freq */
-		sg_policy->cached_raw_freq = 0;
+		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 		return;
 	}
 
@@ -187,6 +187,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 		return sg_policy->next_freq;
 
 	sg_policy->need_freq_update = false;
+	sg_policy->prev_cached_raw_freq = sg_policy->cached_raw_freq;
 	sg_policy->cached_raw_freq = freq;
 	l_freq = cpufreq_driver_resolve_freq(policy, freq);
 	idx = cpufreq_frequency_table_target(policy, freq, CPUFREQ_RELATION_H);
@@ -327,8 +328,9 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	 */
 	if (busy && next_f < sg_policy->next_freq) {
 		next_f = sg_policy->next_freq;
-		/* clear cache when it's bypassed */
-		sg_policy->cached_raw_freq = 0;
+
+		/* Restore cached freq as next_freq has changed */
+		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 	}
 
 	sugov_update_commit(sg_policy, time, next_f);
@@ -802,6 +804,7 @@ static int sugov_start(struct cpufreq_policy *policy)
 	sg_policy->work_in_progress = false;
 	sg_policy->need_freq_update = false;
 	sg_policy->cached_raw_freq = 0;
+	sg_policy->prev_cached_raw_freq	= 0;
 
 	for_each_cpu(cpu, policy->cpus) {
 		struct sugov_cpu *sg_cpu = &per_cpu(sugov_cpu, cpu);
