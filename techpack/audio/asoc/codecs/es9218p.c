7d00ffa68f7568a5337e135cd165938102478ac2
Emanuel <emanuelghub@gmail.com>
2021-11-14 19:32:25 +0400

    techpack: audio: es9218p: Tune for judy family

--- techpack/audio/asoc/codecs/es9218p.c
+++ techpack/audio/asoc/codecs/es9218p.c
@@ -341,6 +341,12 @@ static int g_debug_delay = 500; // ESS pop-click debugging step time delay
 #endif
 static u8  normal_harmonic_comp_left[4] = {0x78, 0x00, 0x9a, 0xfc};
 static u8  normal_harmonic_comp_right[4] = {0x1e, 0x00, 0x12, 0xfd};
+#ifdef CONFIG_MACH_SDM845_JUDY
+static u8 advance_harmonic_comp_left[4] = {0xfe, 0x01, 0x32, 0x00};
+static u8 advance_harmonic_comp_right[4] = {0x9a, 0x01, 0x64, 0x00};
+static u8 aux_harmonic_comp_left[4] = {0x1b, 0x01, 0x0c, 0xfe};
+static u8 aux_harmonic_comp_right[4] = {0xd2, 0x00, 0x34, 0xfe};
+#endif
 #ifdef CONFIG_MACH_SM8150_ALPHA
 static u8  advance_harmonic_comp_left[4] = {0x58, 0x02, 0x3c, 0x00};
 static u8  advance_harmonic_comp_right[4] = {0x21, 0x02, 0x64, 0x00};
@@ -368,11 +374,6 @@ static u8  advance_harmonic_comp_left[4] = {0xa0, 0x02, 0x30, 0x00};
 static u8  advance_harmonic_comp_right[4] = {0x28, 0x02, 0x30, 0x00};
 static u8  aux_harmonic_comp_left[4] = {0x7a, 0x01, 0xe0, 0xfd};
 static u8  aux_harmonic_comp_right[4] = {0x05, 0x01, 0x1f, 0xfe};
-#else
-static u8  advance_harmonic_comp_left[4] = {0x30, 0x02, 0x3c, 0x00};
-static u8  advance_harmonic_comp_right[4] = {0xd6, 0x01, 0x3c, 0x00};
-static u8  aux_harmonic_comp_left[4] = {0x72, 0x01, 0x84, 0xfe};
-static u8  aux_harmonic_comp_right[4] = {0x3b, 0x01, 0x98, 0xfe};
 #endif
 
 enum {
