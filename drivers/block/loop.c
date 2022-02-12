588babf8ed9de0caace598abc8cfff0982a01924
EmanuelCN <emanuelghub@gmail.com>
2022-02-12 19:07:22 +0100

    Revert "fix"
    This reverts commit 4e5a5242200841ec0347e67c2255749e56bde16e.

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -889,7 +889,7 @@ static int loop_prepare_queue(struct loop_device *lo)
 {
 	kthread_init_worker(&lo->worker);
 	lo->worker_task = kthread_run_perf_critical(
-			kthread_worker_fn, &lo->worker,
+			loop_kthread_worker_fn, &lo->worker,
 			"loop%d", lo->lo_number);
 	if (IS_ERR(lo->worker_task))
 		return -ENOMEM;
