733e7cdc1c2330553af55e0d56fc51ed4c003bcf
EmanuelCN <emanuelghub@gmail.com>
2024-04-16 22:17:24 +0300

    Revert "cpu_input_boost: Expose dynamic sched_boost node"
    This reverts commit 937866536fb87e515184d4e1cd7d4aca46b1f112.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -37,8 +37,6 @@ static unsigned short input_boost_duration __read_mostly =
 	CONFIG_INPUT_BOOST_DURATION_MS;
 static unsigned short wake_boost_duration __read_mostly =
 	CONFIG_WAKE_BOOST_DURATION_MS;
-	
-static bool dynamic_sched_boost __read_mostly = true;
 
 module_param(input_boost_freq_little, uint, 0644);
 module_param(input_boost_freq_big, uint, 0644);
@@ -50,7 +48,6 @@ module_param(cpu_freq_min_big, uint, 0644);
 module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
 
-module_param(dynamic_sched_boost, bool, 0644);
 enum {
 	SCREEN_OFF,
 	INPUT_BOOST,
@@ -133,8 +130,7 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
-	if (dynamic_sched_boost)
-		sched_set_boost(2);
+	sched_set_boost(2);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
 			      msecs_to_jiffies(input_boost_duration))) {
 		set_bit(INPUT_BOOST, &b->state);
@@ -190,8 +186,7 @@ static void input_unboost_worker(struct work_struct *work)
 					   typeof(*b), input_unboost);
 
 	clear_bit(INPUT_BOOST, &b->state);
-	if (dynamic_sched_boost)
-		sched_set_boost(0);
+	sched_set_boost(0);
 	wake_up(&b->boost_waitq);
 }
 
@@ -333,8 +328,7 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
-	if (dynamic_sched_boost)
-		sched_set_boost(0);
+	sched_set_boost(0);
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
